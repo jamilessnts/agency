3c67f6901aa6b0feaa71e2e393d51db974170ab1
Jamile Santos <jamile.ssnts>
2018-08-01 23:38:47 -0300

    Component Services receiving a serviceItem working, but still missing some configuration related to placeholder teste

---
+++ src/Feature/Services/code/Controllers/ServiceContainerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc;
+using Glass.Mapper.Sc;
+using Glass.Mapper.Sc.Web.Mvc;
+using Agency.Feature.Services.Models;
+
+namespace Agency.Feature.Services.Controllers
+{
+    public class ServiceContainerController : GlassController
+    {
+        // GET: ServiceContainer
+        public ActionResult ServiceContainer()
+        {
+            var services = this.GetDataSourceItem<ServiceContainer>();
+            return View(services);
+            
+        }
+    }
+}
---
+++ src/Feature/Services/code/Models/ServiceContainer.cs
@@ -0,0 +1,14 @@
+﻿using Agency.Foundation.Orm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agency.Feature.Services.Models
+{
+    public class ServiceContainer: SitecoreItem
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+    }
+}
