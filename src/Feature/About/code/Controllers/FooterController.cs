a81f0f40bba2cd802646ac0376ab64cbba01892f
Jamile Santos <jamile.ssnts>
2018-08-07 15:36:13 -0300

    Just to save

---
+++ src/Feature/About/code/Controllers/FooterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Agency.Feature.About.Models;
+using Glass.Mapper.Sc.Web.Mvc;
+
+namespace Agency.Feature.About.Controllers
+{
+    public class FooterController : GlassController
+    {
+        // GET: Footer
+        public ActionResult Footer()
+        {
+            var footer = this.GetDataSourceItem<Footer>();
+            return View(footer);
+        }
+    }
+}
---
+++ src/Feature/About/code/Models/Footer.cs
@@ -0,0 +1,22 @@
+﻿using Agency.Foundation.Orm.Models;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agency.Feature.About.Models
+{
+    public class Footer: SitecoreItem
+    {
+        public virtual string Copyright { get; set; }
+        public virtual Link Twitter { get; set; }
+        public virtual Link Facebook { get; set; }
+        public virtual Link Linkedin { get; set; }
+        public virtual Link Privacy { get; set; }
+        [SitecoreField("Term of Use")]
+        public virtual Link TermOfUse { get; set; }
+
+    }
+}
