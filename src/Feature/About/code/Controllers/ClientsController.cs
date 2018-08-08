4d0c2c4056b37a18fdc4e67fa89bf4e328333e4e
Jamile Santos <jamile.ssnts>
2018-08-08 13:36:39 -0300

    Clients Component included.

---
+++ src/Feature/About/code/Controllers/ClientsController.cs
@@ -0,0 +1,27 @@
+﻿using Glass.Mapper.Sc.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Agency.Feature.About.Models;
+
+namespace Agency.Feature.About.Controllers
+{
+    public class ClientsController : GlassController
+    {
+        // GET: Sponsor
+        public ActionResult ClientContainer()
+        {
+            var clientContainer = this.GetDataSourceItem<Clients>();
+            return View(clientContainer);
+        }
+
+
+        public ActionResult Clients()
+        {
+            var clients = this.GetDataSourceItem<Clients>();
+            return View(clients);
+        }
+    }
+}
---
+++ src/Feature/About/code/Models/Clients.cs
@@ -0,0 +1,14 @@
+﻿using Agency.Foundation.Orm.Models;
+using Glass.Mapper.Sc.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agency.Feature.About.Models
+{
+    public class Clients: SitecoreItem
+    {
+        public virtual Image ImageClient { get; set; }
+    }
+}
